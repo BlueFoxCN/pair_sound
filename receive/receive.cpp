0d78a17f89adb20beb3c2052659aa723de29d356
JesseYang <jesse.yang1985@gmail.com>
2015-04-11 17:53:23 +0800

    update

--- receive/receive.cpp
+++ receive/receive.cpp
@@ -129,23 +129,17 @@ void Receive::start() {
     if (adpcm) {
       r = recvfrom(fd, adpcm_buffer, size / 4, 0, (struct sockaddr*)&from, &len);
       for (int i = 0; i < size / 4; i++) {
-        log_warn("1111111111111");
         code = ( (short)adpcm_buffer[i] ) & 15;
-        // code = ((short)0 << 8) | adpcm_buffer[i];
-        // code = adpcm_buffer[i];
-        log_warn("*************");
         if ((code & 8) != 0) {
           sb = 1;
         } else {
           sb = 0;
         }
-        log_warn("2222222222222");
         code &= 7;
         delta = (step_table[index]*code) / 4 + step_table[index] / 8;
         if (sb == 1) {
           delta = -delta;
         }
-        log_warn("3333333333333");
         cur_sample += delta;
         if (cur_sample > 32767) {
           cur_data = 32767;
@@ -154,31 +148,26 @@ void Receive::start() {
         } else {
           cur_data = cur_sample;
         }
-        log_warn("44444444444444");
         index += index_adjust[code];
         if (index < 0) {
           index = 0;
         } else if (index > 88) {
           index = 88;
         }
-        log_warn("55555555555555");
         buffer[i * 4] = cur_data;
         buffer[i * 4 + 1] = cur_data >> 8;
 
-        log_warn("66666666666666");
         code = ( (short)adpcm_buffer[i] >> 8) & 15;
         if ((code & 8) != 0) {
           sb = 1;
         } else {
           sb = 0;
         }
-        log_warn("77777777777777");
         code &= 7;
         delta = (step_table[index]*code) / 4 + step_table[index] / 8;
         if (sb == 1) {
           delta = -delta;
         }
-        log_warn("888888888888888");
         cur_sample += delta;
         if (cur_sample > 32767) {
           cur_data = 32767;
@@ -187,18 +176,15 @@ void Receive::start() {
         } else {
           cur_data = cur_sample;
         }
-        log_warn("999999999999999");
         index += index_adjust[code];
         if (index < 0) {
           index = 0;
         } else if (index > 88) {
           index = 88;
         }
-        log_warn("000000000000000");
         buffer[i * 4 + 2] = cur_data;
         buffer[i * 4 + 3] = cur_data >> 8;
       }
-      log_warn("AAAAAAAAAAAA");
       log_warn("1: %d", (((short)buffer[2 * 0 + 1]) << 8) | buffer[2 * 0]);
       log_warn("2: %d", (((short)buffer[2 * 1 + 1]) << 8) | buffer[2 * 1]);
       log_warn("3: %d", (((short)buffer[2 * 2 + 1]) << 8) | buffer[2 * 2]);
